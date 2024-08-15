684c2610d478b04b90306699272098874765a728
WebWithAstha <lodhiastha85@gmail.com>
2024-08-15 23:23:49 +0530

    test commit3

--- src/components/services/ServiceCard.jsx
+++ src/components/services/ServiceCard.jsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from 'react';
 
 const ServiceCard = ({ icon, title, description }) => {
   return(
   <div className="dark:bg-zinc-700 bg-zinc-100 p-4 rounded-lg">
-    <img src={icon} alt={title} className="w-16 h-16 object-contain mb-4" />
+    <Image src={icon} alt={title} className="w-16 h-16 object-contain mb-4" />
     <h3 className="text-xl font-bold mb-2">{title}</h3>
     <p className="dark:text-gray-400 text-sm">{description}</p>
   </div>
